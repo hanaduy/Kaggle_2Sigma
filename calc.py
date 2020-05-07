0e36047d401cc313bff64c57be6b6745ea6452de
Yanda Hu <hanaduy@foxmail.com>
2020-05-06 23:35:34 -0500

    Combining add divide and delete comment
    add divide
    
    no comment

--- calc.py
+++ calc.py
@@ -1,9 +1,3 @@
-2018/11/1 
-Looked at the kernel of the competition and tried to play around with it. 
-Leetcode 104 111, using recursion and non-recursion BFS & DFS
-2018/11/5
-Finished the kernel and understood the kernel. Finished 62,63,110
-
 def add(x,y):
 	pass
 
@@ -16,3 +10,5 @@ def subtract(x,y):
 def printing(x,y):
 	pass
 
+def divide(x,y):
+	pass
